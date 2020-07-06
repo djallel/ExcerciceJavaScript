2fd0e9815f56ae9e1623aafc32a87bfc834958d2
TABET DJALAL <b_tabet@esi.dz>
2020-07-07 00:57:14 +0200

    Excercice facile

---
+++ src/main/resources/JavaScript/Exercices Faciles/Exercice3-read-line-2.js
@@ -0,0 +1,15 @@
+var readline = require("readline-sync");
+
+var nom = readline.question("Quel est votre nom ? ");
+var prenom = readline.question("Quel est votre prenom ? ");
+var age = parseInt(readline.question("Quel est votre age ? "));
+
+var txt = "--------------------------------- \n";
+txt += "-----------BIENVENUE------------- \n";
+txt += "--------------------------------- \n";
+
+txt += "Votre nom est : "+nom+"\n";
+txt += "Votre prenom est : "+prenom+"\n";
+txt += "Vous avez "+age+" ans\n";
+
+console.log(txt);
---
+++ src/main/resources/JavaScript/Exercices Faciles/Exercice8-Tests sur String.js
@@ -0,0 +1,13 @@
+function afficherTaille(mot){
+    if(mot.length < 8) {
+        console.log("Le mot contient " + mot.length + " caracteres");
+    } else {
+        console.log("Le mot fait plus de 8 caracteres");
+    }
+}
+
+var mot1 = "Marc";
+var mot2 = "Gwendoline";
+
+afficherTaille(mot1);
+afficherTaille(mot2);
