40a5b4c97e9a9f3a6b6d1a00d0d9cc1be7eb0ee8
TABET DJALAL <b_tabet@esi.dz>
2020-07-15 22:55:59 +0200

    Excercice Difficil

---
+++ src/main/resources/JavaScript/Exercices Difficiles/Gestion d'arme/armeManager.js
@@ -0,0 +1,38 @@
+var readline = require("readline-sync");
+var fs = require("fs");
+var types = require("./json/types.json")
+var armes = require("./json/armes.json")
+
+var armeManager = {
+    afficherArmes : function(){
+        for(var arme in armes){
+            if(arme.substr(0,4) === "arme"){
+                var idArme = arme.substr(-1,1);
+                console.log(idArme + " : " +this.afficherArme(armes[arme]));
+            }
+        }
+    },
+    afficherArme : function(arme){
+        return ("Nom : " + arme.nom + " - Type : " + types[arme.type]);
+    },
+    ajouterArme : function(){
+        var nouvelleArme = {};
+        nouvelleArme.nom = readline.question("Quel est le nom de l'arme que vous voulez ajouter ? ");
+        this.afficherTypes();
+        nouvelleArme.type = parseInt(readline.question("Quel est son type ? "));
+        armes.increment++;
+        armes["arme"+armes.increment] = nouvelleArme;
+        fs.writeFileSync("./json/armes.json",JSON.stringify(armes,undefined,4));
+    },
+    afficherTypes : function(){
+        for(var type in types) {
+            console.log(type+" : " + types[type]);
+        }
+    },
+    supprimerArme : function(){
+        var armeASupprimer = readline.question("Quel est le numéro de l'arme que vous voulez supprimer ? ");
+        delete armes["arme"+armeASupprimer];
+        fs.writeFileSync("./json/armes.json",JSON.stringify(armes));
+    }
+}
+module.exports = armeManager;
