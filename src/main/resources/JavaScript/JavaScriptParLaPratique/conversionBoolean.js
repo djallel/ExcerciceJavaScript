07e12985419e954193ab919fbc96039ff6d901f9
TABET DJALAL <b_tabet@esi.dz>
2020-07-26 16:44:17 +0200

    commit all projects

---
+++ src/main/resources/JavaScript/JavaScriptParLaPratique/conversionBoolean.js
@@ -0,0 +1,7 @@
+var isRich= true;
+typeof isRich;
+console.log(`typeof isRich => `+typeof isRich) // envoi boolean
+console.log(`typeof null => `+typeof null) // envoi object
+console.log(`typeof undefined => `+typeof undefined) // envoi undefined
+console.log(`typeof [] => `+typeof []) // envoi object
+console.log(`typeof [undefined] => `+typeof [undefined]) // envoi object
