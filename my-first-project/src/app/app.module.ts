a6824b3e3e3b6ef15f09f1dc9ed3fb87b748cfab
TABET DJALAL <b_tabet@esi.dz>
2020-08-25 13:54:47 +0200

    guide Material Design

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatSliderModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
