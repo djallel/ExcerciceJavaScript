a26730fb44c2fcbfe41f65c551e58563d2752dff
TABET DJALAL <b_tabet@esi.dz>
2020-09-15 12:51:46 +0200

    string interpolation+resumé javaScript

--- cvTabet/src/app/contact/contact.component.ts
+++ cvTabet/src/app/contact/contact.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  situationMartial = "Célibataire.";
+  nombreEnfant = "0 Enfants."
+  typePermis = "Permis B."
+  age = "30 ans";
+  mel = "b_tabet@esi.dz"
+  tel = "+33 (0) 7 68 01 03 64"
+  nom="TABET Belaid Djalal"
+  mobilite="Toute la France"
+  skype="contact.tabet"
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getSituationMatrial (){
+    return this.situationMartial
   }
 
 }
